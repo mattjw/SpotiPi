e1f04caf433e5fe803f59d3b389c5c27eb823caa
System Administrator <root@m0363.comsc.cu-wifi.cf.ac.uk>
2012-03-10 18:54:59 +0000

    Listing playlists and jquery in use

--- cfhack/web/examplesite/mks/forms.py
+++ cfhack/web/examplesite/mks/forms.py
@@ -1,9 +1,17 @@
-from django.forms import ModelForm
-from mks.models import User
+from django.forms import ModelForm, ModelChoiceField
+from mks.models import User, Playlists
 from django import forms
 
 class UserForm(ModelForm):
     name  = forms.CharField(label="Your Username" )
     password = forms.CharField( widget=forms.PasswordInput, label="Your Password" )
+    default_pl = ModelChoiceField(label="Default Playlist", queryset=Playlists.objects.values_list('name'), required=False)
     class Meta:
         model = User
+
+class PartialUserForm(ModelForm):
+    name  = forms.CharField(label="Your Username" )
+    password = forms.CharField( widget=forms.PasswordInput, label="Your Password" )
+    class Meta:
+        model = User
+        exclude = ('default_pl',)
--- cfhack/web/examplesite/mks/models.py
+++ cfhack/web/examplesite/mks/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django import forms
 
+class Playlists(models.Model):
+	pl_id = models.CharField(primary_key=True, max_length=10)
+	name = models.CharField(max_length=100)
+
 class User(models.Model):
     name = models.CharField(max_length=20)
     password = models.CharField(max_length=10)
+    default_pl = models.ForeignKey(Playlists, null=True)
     def __unicode__(self):
         return self.name
